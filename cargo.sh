da2846ee0b734512134b74b3550c75d94e66dec2
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2014-07-21 14:21:13 +0000

    Detect cargo

---
+++ cargo.sh
@@ -0,0 +1,2 @@
+test ! -z `which cargo` && export CARGO=`which cargo`
+test -x ../cargo/target/cargo && export CARGO=../cargo/target/cargo
--- handle.sh
+++ handle.sh
@@ -1,3 +1,4 @@
-#!/bin/sh -x
-../cargo/target/cargo build \
+#!/bin/bash -x
+source ./cargo.sh
+$CARGO build \
   && env RUST_LOG=handle=debug target/handle
